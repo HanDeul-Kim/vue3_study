42b484e34ff4d906c4daf521d56a165f180db85d
김한들 <dle1883@gmail.com>
2025-11-25 15:08:20 +0900

    v-model로 수량 input 기능 추가

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,7 +3,7 @@
         <div class="modal-overlay" @click="closeModal"></div>
         <div class="modal">
             <!-- <button @click="closeModal" class="modal-close">닫기</button> -->
-            
+
             <!-- $emit -->
             <button @click="$emit('close-modal')" class="modal-close">
                 닫기
@@ -21,7 +21,10 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <span>{{ products[visibleModal].price }} won</span>
+                <input v-model.number="quantity" type="text" class="input-32" placeholder="수량 input">
+                <!-- <span v-if="quantity == ''">{{products[visibleModal].price}} won</span>
+                <span v-else>{{ Number(products[visibleModal]?.price.replace(/,/g, '')) * quantity }} won</span> -->
+                <span>{{ finalPrice() }} won</span>
             </div>
         </div>
     </div>
@@ -30,13 +33,29 @@
 <script>
 export default {
     name: 'Modal',
+    data() {
+        return {
+            quantity: "",
+        }
+    },
     props: {
-        products : Array,
+        products: Array,
         visibleModal: Boolean,
         closeModal: Function
     },
-    data() {
-        return {}
+    methods: {
+        // 수량에 의한 가격 결과값
+        finalPrice() {
+            if (isNaN(Number(this.quantity))) {
+                return this.products[this.visibleModal].price;
+            } else {
+                const priceStr = this.products[this.visibleModal].price;
+                const priceNum = Number(priceStr.replace(/,/g, ''));
+                const qty = this.quantity === "" ? 1 : Number(this.quantity);
+                return (priceNum * qty).toLocaleString();
+            }
+
+        }
     },
 }
 </script>
